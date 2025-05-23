2d89d81c60fa36ef0ccbf14249492749c6f1d902
faustinopsy <rodrigohipnose@gmail.com>
2025-05-23 19:18:55 -0300

    add desenha fundo e trocando os nomes dos metodos do canvas

--- js/componentes/DesenhoCanvas.js
+++ js/componentes/DesenhoCanvas.js
@@ -2,14 +2,22 @@ export default class DesenhoCanvas {
     constructor(canvasId) {
         this.canvas = canvasId;
         this.ctx = this.canvas.getContext('2d');
-        this.desenhar();
+        this.desenharChao();
+        this.desenharCeu();
     }
 
-    desenhar() {
+    desenharCeu() {
+        if (!this.ctx) return;
+        this.ctx.fillStyle = '#ADD8E6';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height - 50);
+    }
+
+    desenharChao() {
         console.log("Desenhando no canvas");
         this.ctx.fillStyle = '#8B4513';
         this.ctx.fillRect(0, 250, this.canvas.width, 200);
     }
+
 
    
 }
