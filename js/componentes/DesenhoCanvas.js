2f3468b10edb0fda82c2460d0b48376a9cbb2394
faustinopsy <rodrigohipnose@gmail.com>
2025-05-23 22:10:47 -0300

    add lava chao

--- js/componentes/DesenhoCanvas.js
+++ js/componentes/DesenhoCanvas.js
@@ -10,11 +10,12 @@ export default class DesenhoCanvas {
         this.larguraCasa = 80;
         this.alturaCasa = 60;
         this.alturaTeto = 50;
-        this.distanciVulcao = this.centroVulcao + 500;
-        this.posicaoY = 190; 
+        this.posicaoXVila = this.centroVulcao + 500;
+        this.posicaoYCasaTopo = 190; 
         
         this.larguraTopoFluxoLava = 20; 
         this.larguraBaseFluxoLavaNaEncosta = 30; 
+        this.espessuraLavaHorizontal = 25; 
     }
 
     desenharCeu() {
@@ -43,34 +44,48 @@ export default class DesenhoCanvas {
     desenharVila() {
         if (!this.ctx) return;
         this.ctx.fillStyle = '#3F9977';
-        this.ctx.fillRect(this.distanciVulcao, this.posicaoY, this.larguraCasa, this.alturaCasa);
+        this.ctx.fillRect(this.posicaoXVila, this.posicaoYCasaTopo, this.larguraCasa, this.alturaCasa);
         this.ctx.fillStyle = '#8F0077';
         this.ctx.beginPath();
-        this.ctx.moveTo(this.distanciVulcao, this.posicaoY);
-        this.ctx.lineTo(this.distanciVulcao + this.larguraCasa / 2, this.posicaoY - this.alturaTeto);
-        this.ctx.lineTo(this.distanciVulcao + this.larguraCasa, this.posicaoY);
+        this.ctx.moveTo(this.posicaoXVila, this.posicaoYCasaTopo);
+        this.ctx.lineTo(this.posicaoXVila + this.larguraCasa / 2, this.posicaoYCasaTopo - this.alturaTeto);
+        this.ctx.lineTo(this.posicaoXVila + this.larguraCasa, this.posicaoYCasaTopo);
         this.ctx.closePath();
         this.ctx.fill();
     }
 
     desenhaLavaDescendo() {
         if (!this.ctx) return;
-
         this.ctx.fillStyle = 'orangered';
         this.ctx.beginPath();
-
         const pontoPicoX = this.centroVulcao;
         const pontoPicoY = this.altura;
         const pontoBaseDireitaVulcaoX = this.centroVulcao + this.larguraBase;
         const pontoBaseDireitaVulcaoY = this.baseY;
-
         this.ctx.moveTo(pontoPicoX - this.larguraTopoFluxoLava / 2, pontoPicoY);
         this.ctx.lineTo(pontoPicoX + this.larguraTopoFluxoLava / 2, pontoPicoY);
         this.ctx.lineTo(pontoBaseDireitaVulcaoX, pontoBaseDireitaVulcaoY);
         this.ctx.lineTo(pontoBaseDireitaVulcaoX - this.larguraBaseFluxoLavaNaEncosta, pontoBaseDireitaVulcaoY);
-        
         this.ctx.closePath();
         this.ctx.fill();
+    }
+
+    desenhaLavaChao() {
+        if (!this.ctx) return;
+
+        const xInicioLavaHorizontal = this.centroVulcao + this.larguraBase - this.larguraBaseFluxoLavaNaEncosta;
+        const xFimLavaHorizontal = this.posicaoXVila;
+        const larguraLavaHorizontal = xFimLavaHorizontal - xInicioLavaHorizontal;
+
+        if (larguraLavaHorizontal > 0) {
+            this.ctx.fillStyle = 'orangered';
+            this.ctx.fillRect(
+                xInicioLavaHorizontal,
+                this.baseY - this.espessuraLavaHorizontal,
+                larguraLavaHorizontal,
+                this.espessuraLavaHorizontal
+            );
+        }
     }
 
     limparCanvas() {
@@ -86,5 +101,6 @@ export default class DesenhoCanvas {
         this.desenharVulcao();
         this.desenharVila();
         this.desenhaLavaDescendo();
+        this.desenhaLavaChao();
     }
 }
